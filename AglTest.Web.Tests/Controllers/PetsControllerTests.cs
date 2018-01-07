c2415a123e9a955a1f9b0627d8f4a82973ac1e8c
Faraj Farook <farajfarook@gmail.com>
2018-01-07 21:26:18 +1100

    Adding Web project unit tests

---
+++ AglTest.Web.Tests/Controllers/PetsControllerTests.cs
@@ -0,0 +1,13 @@
+using Xunit;
+
+namespace AglTest.Web.Tests.Controllers
+{
+    public class PetsControllerTests
+    {
+        [Fact]
+        public void Get_AllSuccess()
+        {
+            
+        }
+    }
+}
