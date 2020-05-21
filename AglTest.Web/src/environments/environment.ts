76cfe99983ce53b90b1db9a71f567a8a585acb00
Faraj Farook <farajfarook@gmail.com>
2020-05-21 21:02:28 +1000

    change default to have local API

--- AglTest.Web/src/environments/environment.ts
+++ AglTest.Web/src/environments/environment.ts
@@ -1,8 +1,8 @@
 export const environment = {
   production: false,
   api: {
-    //host: 'http://localhost:5000/',
-    host: 'http://localhost:8088/api/v1/',
+    host: 'http://localhost:5000/',
+    //host: 'http://localhost:8088/api/v1/',
     services: {
       pets: 'pets'
     }
