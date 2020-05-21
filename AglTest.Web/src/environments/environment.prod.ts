f8e678c03bcd3dc77bc5d38cf6d6423a584934fb
Faraj Farook <farajfarook@gmail.com>
2020-05-21 15:36:59 +1000

    Initial commit of Angular Front end

---
+++ AglTest.Web/src/environments/environment.prod.ts
@@ -0,0 +1,9 @@
+export const environment = {
+  production: true,
+  api: {
+    server: 'http://localhost:8088/',
+    services: {
+      data: "api/v1"
+    }
+  }
+}
---
+++ AglTest.Web/src/environments/environment.ts
@@ -0,0 +1,9 @@
+export const environment = {
+  production: false,
+  api: {
+    server: 'http://localhost:8088/',
+    services: {
+      data: "api/v1"
+    }
+  }
+}
